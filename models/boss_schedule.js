e8dfbd67f6b256035a6650ae47729c84f6cf4f4b
schstr <ricardo@schstr.com>
2022-02-05 09:51:34 -0300

    Varias coisa

--- models/boss_schedule.js
+++ models/boss_schedule.js
@@ -1,50 +1,50 @@
 const bosses = {
     monday:[
-        {time: '02:00', boss: 'Karanda & Kzarka', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '02:0', boss: 'Karanda & Kzarka', before15: '01:45', before10: '01:50', before5: '01:55'},
         {time: '11:00', boss: 'Nouver', before15: '10:45', before10: '10:50', before5: '10:55'},
         {time: '16:00', boss: 'Kutum & Kzarka', before15: '15:45', before10: '15:50', before5: '15:55'},
         {time: '20:00', boss: 'Nouver & Kutum', before15: '19:45', before10: '19:50', before5: '19:55'},
     ],
     tuesday: [
-        {time: '02:00', boss: 'Kutum', before15: '01:45', before10: '01:50', before5: '01:55'},
-        {time: '11:00', boss: 'Kzarka', before15: '10:45', before10: '10:50', before5: '10:55'},
-        {time: '16:00', boss: 'Nouver & Kutum', before15: '15:45', before10: '15:50', before5: '15:55'},
-        {time: '20:00', boss: 'Kzarka & Karanda', before15: '19:45', before10: '19:50', before5: '19:55'},
+        {time: '2:0', boss: 'Kutum', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '11:0', boss: 'Kzarka', before15: '10:45', before10: '10:50', before5: '10:55'},
+        {time: '16:0', boss: 'Nouver & Kutum', before15: '15:45', before10: '15:50', before5: '15:55'},
+        {time: '20:0', boss: 'Kzarka & Karanda', before15: '19:45', before10: '19:50', before5: '19:55'},
         {time: '23:45', boss: 'Offin', before15: '23:30', before10: '23:35', before5: '23:40'}, 
     ],
     wednesday:[
-        {time: '02:00', boss: 'Kzarka', before15: '01:45', before10: '01:50', before5: '01:55'},
-        {time: '11:00', boss: 'Kutum & Karanda', before15: '10:45', before10: '10:50', before5: '10:55' },
-        {time: '16:00', boss: 'Kzarka & Nouver', before15: '15:45', before10: '15:50', before5: '15:55'},
-        {time: '20:00', boss: 'Quint & Muraka', before15: '19:45', before10: '19:50', before5: '19:55'},
+        {time: '02:0', boss: 'Kzarka', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '11:0', boss: 'Kutum & Karanda', before15: '10:45', before10: '10:50', before5: '10:55' },
+        {time: '16:0', boss: 'Kzarka & Nouver', before15: '15:45', before10: '15:50', before5: '15:55'},
+        {time: '20:0', boss: 'Quint & Muraka', before15: '19:45', before10: '19:50', before5: '19:55'},
         {time: '23:45', boss: 'Garmoth', before15: '23:30', before10: '23:35', before5: '23:40'}
     ],
     thursday: [
-        {time: '02:00', boss: 'Karanda & Kutum', before15: '01:45', before10: '01:50', before5: '01:55'},
-        {time: '11:00', boss: 'Nouver & Karanda', before15: '10:45', before10: '10:50', before5: '10:55'},
-        {time: '16:00', boss: 'Kutum & Kzarka', before15: '15:45', before10: '15:50', before5: '15:55'},
-        {time: '20:00', boss: 'Nouver & Kutum', before15: '19:45', before10: '19:50', before5: '19:55'},
+        {time: '02:0', boss: 'Karanda & Kutum', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '11:0', boss: 'Nouver & Karanda', before15: '10:45', before10: '10:50', before5: '10:55'},
+        {time: '16:0', boss: 'Kutum & Kzarka', before15: '15:45', before10: '15:50', before5: '15:55'},
+        {time: '20:0', boss: 'Nouver & Kutum', before15: '19:45', before10: '19:50', before5: '19:55'},
     ],
     friday: [
         {time: '00:15', boss: 'Vell'},
-        {time: '02:00', boss: 'Karanda & Offin', before15: '01:45', before10: '01:50', before5: '01:55'},
-        {time: '11:00', boss: 'Nouver', before15: '10:45', before10: '10:50', before5: '10:55'},
-        {time: '16:00', boss: 'Kutum & Kzarka', before15: '15:45', before10: '15:50', before5: '15:55'},
-        {time: '20:00', boss: 'Nouver & Kutum', before15: '19:45', before10: '19:50', before5: '19:55'},
+        {time: '02:0', boss: 'Karanda & Offin', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '11:0', boss: 'Nouver', before15: '10:45', before10: '10:50', before5: '10:55'},
+        {time: '16:0', boss: 'Kutum & Kzarka', before15: '15:45', before10: '15:50', before5: '15:55'},
+        {time: '20:0', boss: 'Nouver & Kutum', before15: '19:45', before10: '19:50', before5: '19:55'},
         {time: '23:45', boss: 'Garmoth', before15: '23:30', before10: '23:35', before5: '23:40'}
     ],
     saturday: [
-        {time: '02:00', boss: 'Kzarka', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '02:0', boss: 'Kzarka', before15: '01:45', before10: '01:50', before5: '01:55'},
         {time: '11:00', boss: 'Kzarka & Karanda', before15: '10:45', before10: '10:50', before5: '10:55'},
         {time: '16:00', boss: 'Nouver & Kutum', before15: '15:45', before10: '15:50', before5: '15:55'},
         {time: '20:00', boss: 'Quint & Muraka', before15: '19:45', before10: '19:50', before5: '19:55'},
     ],
-    monday: [
-        {time: '02:00', boss: 'Nouver', before15: '01:45', before10: '01:50', before5: '01:55'},
-        {time: '11:00', boss: 'Kutum & Nouver', before15: '10:45', before10: '10:50', before5: '10:55'},
-        {time: '16:00', boss: 'Kzarka & Karanda', before15: '15:45', before10: '15:50', before5: '15:55'},
-        {time: '18:00', boss: 'Vell', before15: '17:45', before10: '17:50', before5: '17:55'},
-        {time: '20:00', boss: 'Nouver & Karanda', before15: '19:45', before10: '19:50', before5: '19:55'},
+    sunday: [
+        {time: '02:0', boss: 'Nouver', before15: '01:45', before10: '01:50', before5: '01:55'},
+        {time: '11:0', boss: 'Kutum & Nouver', before15: '10:45', before10: '10:50', before5: '10:55'},
+        {time: '16:0', boss: 'Kzarka & Karanda', before15: '15:45', before10: '15:50', before5: '15:55'},
+        {time: '18:0', boss: 'Vell', before15: '17:45', before10: '17:50', before5: '17:55'},
+        {time: '20:0', boss: 'Nouver & Karanda', before15: '19:45', before10: '19:50', before5: '19:55'},
         {time: '23:45', boss: 'Offin', before15: '23:30', before10: '23:35', before5: '23:40'}
     ]
 }
--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,78 +1,80 @@
 const Event = require('../../structures/Event');
 const cron = require('node-cron');
 const { DateTime } = require("luxon");
-const { bosses } = require ('../../../models/boss_schedule')
+const { bosses } = require('../../../models/boss_schedule')
 
 
 module.exports = class extends Event {
-    constructor(client){
+    constructor(client) {
         super(client, {
             name: 'ready'
         })
     }
 
-    run =  () => {
+    run = () => {
         //Bot Login
         this.client.registerCommands()
         console.log(`Bot logged in as ${this.client.user.username} in ${this.client.guilds.cache.size} servers`)
 
-        const CHANNEL = this.client.channels.cache.find(channel => channel.id === process.env.BT_CHANNEL)
-        const DAY = DateTime.now().weekdayLong.toLocaleLowerCase()
+        cron.schedule('* * * * *', () => {
 
-        //Bot Sets Schedule and Timezone
-        function getHours(timeAdd){
-            let hour
-            let minute
-            
-            if(timeAdd){
-                hour = DateTime.now().setZone(process.env.TIMEZONE).plus({minutes: timeAdd}).c.hour
-                minute = DateTime.now().setZone(process.env.TIMEZONE).plus({minutes: timeAdd}).c.minute
-            }else{
-                hour= DateTime.now().setZone(process.env.TIMEZONE).c.hour
-                minute = DateTime.now().setZone(process.env.TIMEZONE).c.minute
-            }
-            return `${hour}:${minute}`
-        }
+            console.log('Rodou!')
 
-        const HOUR = getHours();
-        const FUTURE_15M = getHours(15);
-        const FUTURE_10M = getHours(10);
-        const FUTURE_5M = getHours(5);
+            const CHANNEL = this.client.channels.cache.find(channel => channel.id === process.env.BT_CHANNEL)
+            const DAY = DateTime.now().weekdayLong.toLocaleLowerCase()
 
-        console.log(HOUR, FUTURE_15M, FUTURE_10M, FUTURE_5M)
+            //Bot Sets Schedule and Timezone
+            function getHours(timeAdd) {
+                let hour
+                let minute
 
- 
-        //Bot starts counting timing
-        for(var i = 0; i < bosses[DAY].length; i++){
-            
-            if(bosses[DAY][i].before15 === '19:45' /*FUTURE_15M*/){
-                CHANNEL.send({
-                    content: `@everyone ATENÇÃO MEU PAU VAI TA DURAO EM 15MIN`
-                })
+                if (timeAdd) {
+                    hour = DateTime.now().setZone(process.env.TIMEZONE).plus({ minutes: timeAdd }).c.hour
+                    minute = DateTime.now().setZone(process.env.TIMEZONE).plus({ minutes: timeAdd }).c.minute
+                } else {
+                    hour = DateTime.now().setZone(process.env.TIMEZONE).c.hour
+                    minute = DateTime.now().setZone(process.env.TIMEZONE).c.minute
+                }
+                return `${hour}:${minute}`
             }
 
-            if(bosses[DAY][i].before10 === FUTURE_10M){
-                CHANNEL.send({
-                    content: `@everyone ATENÇÃO MEU PAU VAI TA DURAO EM 10MIN`
-                })
+            const HOUR = getHours();
+            const FUTURE_15M = getHours(15);
+            const FUTURE_10M = getHours(10);
+            const FUTURE_5M = getHours(5);
+
+            console.log(HOUR)
+
+
+            //Bot starts counting timing
+            for (var i = 0; i < bosses[DAY].length; i++) {
+
+                if (bosses[DAY][i].before15 === HOUR /*FUTURE_15M*/) {
+                    CHANNEL.send({
+                        content: `@everyone ATENÇÃO MEU PAU VAI TA DURAO EM 15MIN`
+                    })
+                }
+
+                if (bosses[DAY][i].before10 === HOUR) {
+                    CHANNEL.send({
+                        content: `@everyone ATENÇÃO MEU PAU VAI TA DURAO EM 10MIN`
+                    })
+                }
+
+
+                if (bosses[DAY][i].before5 === HOUR) {
+                    CHANNEL.send({
+                        content: `@everyone ATENÇÃO MEU PAU VAI TA DURAO EM 5MIN`
+                    })
+                }
+
+
+                if (bosses[DAY][i].time === HOUR) {
+                    CHANNEL.send({
+                        content: `@everyone ATENÇÃO MEU PAU TA DURAO`
+                    })
+                }
             }
-
-
-            if(bosses[DAY][i].before5 === FUTURE_5M){
-                CHANNEL.send({
-                    content: `@everyone ATENÇÃO MEU PAU VAI TA DURAO EM 5MIN`
-                })
-            }
-
-
-            if(bosses[DAY][i].time === HOUR){
-                channel.send({
-                    content: `@everyone ATENÇÃO MEU PAU TA DURAO`
-                })
-            }
-
-        }
-
-
+        });
     }
 }
